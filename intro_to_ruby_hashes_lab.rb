ab72cf92789411351e307877f2111095c2bca3d6
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-02 17:21:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -67,5 +67,38 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+  new_hash = {:railroads => {}}
+  
+  new_hash[:railroads] = {:pieces => 4}
+  
+  new_hash[:railroads][:rent_in_dollars] = {}
+  
+  new_hash[:railroads][:names] = {}
+  
+  #------------------
+  
+  new_hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  
+  
+  new_hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  
+  new_hash[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  
+  new_hash[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  
+  new_hash[:railroads][:names][:reading_railroad] = {}
+  
+  new_hash[:railroads][:names][:pennsylvania_railroad] = {}
+  
+  new_hash[:railroads][:names][:b_and_o_railroad] = {}
+  
+  new_hash[:railroads][:names][:shortline_railroad] = {}
+  
+  #-----------------------
+  
+  
+  
+  new_hash
 
 end
