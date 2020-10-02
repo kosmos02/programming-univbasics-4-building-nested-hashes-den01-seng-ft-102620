1ea385ba6173c4b380db092d8b9e9e78fb7a2593
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-02 16:09:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_second_tier
   
   new_hash[:railroads] = {:pieces => 4}
   
-  new_hash[:railroads].push ({:rent_in_dollars => {}})
+  new_hash[:railroads][:rent_in_dollars] = {}
   
   new_hash[:railroads] << {:names => {}}
   
